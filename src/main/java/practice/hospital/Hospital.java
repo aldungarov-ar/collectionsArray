37d6841a1946c258fbc6910d43402dad6c0132e7
Skillbox <it@skillbox.ru>
2022-12-18 20:51:05 +0000

    homework 10 done

--- src/main/java/practice/hospital/Hospital.java
+++ src/main/java/practice/hospital/Hospital.java
@@ -1,24 +1,42 @@
 package practice.hospital;
 
+import java.util.Random;
+
 public class Hospital {
+    public static final int MIN_TEMPERATURE = 32;
+    public static final int MAX_TEMPERATURE = 40;
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
-        return new float[0];
+        float[] temperatures = new float[patientsCount];
+        Random random = new Random();
+        for (int i = 0; i < patientsCount; i++) {
+            temperatures[i] = MIN_TEMPERATURE + random.nextFloat() * (MAX_TEMPERATURE - MIN_TEMPERATURE);
+        }
+        return temperatures;
     }
 
     public static String getReport(float[] temperatureData) {
-        /*
-        TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
-            а также температуры всех пациентов.
-            Округлите среднюю температуру с помощью Math.round до 2 знаков после запятой,
-            а температуры каждого пациента до 1 знака после запятой
-        */
 
-        String report =
-            "Температуры пациентов: " + 1 +
-            "\nСредняя температура: " + 0 +
-            "\nКоличество здоровых: " + 0;
+        StringBuilder patientTemperatures = new StringBuilder();
+        int healthyPatientCounter = 0;
+        float temperatureSum = 0;
 
-        return report;
+        for (float temperatureDatum : temperatureData) {
+            float patientTemperature = (float) Math.round(temperatureDatum * 10) / 10;
+            patientTemperatures.append(patientTemperature).append(" ");
+
+            if (patientTemperature >= 36.2f && patientTemperature <= 36.9f) {
+                healthyPatientCounter++;
+            }
+
+            temperatureSum += temperatureDatum;
+        }
+
+        float averageTemperature = temperatureSum / temperatureData.length;
+        averageTemperature = (float) Math.round(averageTemperature * 100) / 100;
+
+        return "Температуры пациентов: " + patientTemperatures.toString().strip() +
+        "\nСредняя температура: " + averageTemperature +
+        "\nКоличество здоровых: " + healthyPatientCounter;
     }
 }
